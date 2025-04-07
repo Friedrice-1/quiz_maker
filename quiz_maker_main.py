805354801d03c6feafa2e231e5179ba3ff24e83e
Friedrice-1 <allenjayansing99@gmail.com>
2025-04-07 17:29:44 +0800

    Pseudocode

---
+++ quiz_maker_main.py
@@ -0,0 +1,6 @@
+# Initialize an empty list to store questions
+# Initialize an empty list to store choices
+# Loop until user exits
+# Ask user to input a question
+# Ask user to input choices A - D
+# Write the collected data to a text file
