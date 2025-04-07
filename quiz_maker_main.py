24c07d1e43b63bb5b016cd80e5417af450b3926e
Friedrice-1 <allenjayansing99@gmail.com>
2025-04-07 17:36:51 +0800

    Input loops until user exits

--- quiz_maker_main.py
+++ quiz_maker_main.py
@@ -1,6 +1,10 @@
 # Initialize an empty list to store questions
 questions = []
 # Create a loop that breaks if user wants to
+while True:
 # Ask user to input a question
 # Ask user to input choices A - D
+    continue_input = input("Do you want to continue input [Y/N]?: ").upper()
+    if continue_input == "N":
+        break
 # Write the collected data to a text file
