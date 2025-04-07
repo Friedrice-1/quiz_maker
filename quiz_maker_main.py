fb6bcc49cb6e2001ad3ce8b6c86d3ac0e7dc971c
Friedrice-1 <allenjayansing99@gmail.com>
2025-04-07 17:39:38 +0800

    Question and answers input in the loop

--- quiz_maker_main.py
+++ quiz_maker_main.py
@@ -3,7 +3,13 @@ questions = []
 # Create a loop that breaks if user wants to
 while True:
 # Ask user to input a question
+    question = input("Please enter a question: ")
 # Ask user to input choices A - D
+    choice_a = input("Please enter choice 'A': ")
+    choice_b = input("Please enter choice 'B': ")
+    choice_c = input("Please enter choice 'C': ")
+    choice_d = input("Please enter choice 'D': ")
+
     continue_input = input("Do you want to continue input [Y/N]?: ").upper()
     if continue_input == "N":
         break
