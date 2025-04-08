0e930e4768bb4c9dbdd072c7d3c59391728ccd29
Friedrice-1 <allenjayansing99@gmail.com>
2025-04-09 05:07:38 +0800

    Adds questions and choices to a dictionary and appends them to the empty list

--- quiz_maker_main.py
+++ quiz_maker_main.py
@@ -11,8 +11,22 @@ while True:
     choice_d = input("Please enter choice 'D': ").upper()
 
     correct_answer = input("Please enter the correct answer [A/B/C/D]: ").upper()
+    
+    question_data = {
+        "question": question,
+        "choices": {
+            "A": choice_a,
+            "B": choice_b,
+            "C": choice_c,
+            "D": choice_d
+        },
+        "correct": correct_answer
+    }
+    questions.append(question_data)
 
     continue_input = input("Do you want to continue input [Y/N]?: ").upper()
     if continue_input == "N":
         break
+
+print(questions)
 # Write the collected data to a text file
