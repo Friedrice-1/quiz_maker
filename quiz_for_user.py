e7ad153fb3f6ea9c8f2e90e4e43faee476e7370a
Friedrice-1 <allenjayansing99@gmail.com>
2025-04-30 07:45:51 +0800

    Adds the choices including the correct one, into the dictionary

--- quiz_for_user.py
+++ quiz_for_user.py
@@ -11,8 +11,17 @@ def load_quiz(filename="quiz_data.txt"):
     while i < len(lines):
         if lines[i].startswith("Question"):
             question_text = lines[i].strip().split("Question: ")[1]
+            choices = {}
+            for j in range(1,5):
+                line = lines[i+j].strip()
+                key, val = line.split(": ")
+                choices[key] = val
+            correct_line = lines[i + 5].strip()
+            correct_answer = correct_line.split(": ")[1]
             questions.append({
                 "Question": question_text,
+                "Choices": choices,
+                "Correct": correct_answer
             })
             i += 7
         else:
