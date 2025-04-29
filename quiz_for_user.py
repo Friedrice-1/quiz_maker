4e80f31fecbc38595180d177ffe70fd3028c8606
Friedrice-1 <allenjayansing99@gmail.com>
2025-04-30 07:45:09 +0800

    Adds the question from the file into the dictionary

--- quiz_for_user.py
+++ quiz_for_user.py
@@ -2,13 +2,25 @@
 import random
 # function to read the quiz file
 def load_quiz(filename="quiz_data.txt"):
+    questions = []
     with open(filename, "r") as file:
         lines = file.readlines()
-    return lines
+
+# convert the file into a dictionary
+    i = 0
+    while i < len(lines):
+        if lines[i].startswith("Question"):
+            question_text = lines[i].strip().split("Question: ")[1]
+            questions.append({
+                "Question": question_text,
+            })
+            i += 7
+        else:
+            i += 1
+    return questions
 
 quiz = load_quiz()
 print(quiz)
-# convert the file into a dictionary
 # load questions from file
 # shuffle the questions
 # start the quiz
