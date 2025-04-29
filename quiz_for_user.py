93199dad1727fa3808e2447a2e32237ee8cb21ba
Friedrice-1 <allenjayansing99@gmail.com>
2025-04-30 07:36:40 +0800

    Imports random and reads text file

--- quiz_for_user.py
+++ quiz_for_user.py
@@ -1,5 +1,13 @@
 # import random
+import random
 # function to read the quiz file
+def load_quiz(filename="quiz_data.txt"):
+    with open(filename, "r") as file:
+        lines = file.readlines()
+    return lines
+
+quiz = load_quiz()
+print(quiz)
 # convert the file into a dictionary
 # load questions from file
 # shuffle the questions
